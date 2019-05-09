93dc66423efca5359da1f5c0d5ac6b1d9a575993
zhangmin <minzhang@vw-mobvoi.com>
2019-05-09 14:31:13 +0800

    commit 3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import com.mobvoi.vw.common.integration.URLCommonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +11,10 @@ public class DemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Bean
+  public URLCommonService urlCommonService(){
+    return new URLCommonService();
   }
 }
