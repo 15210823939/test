810feba06ed8b28f459f5da04794c6007757d15f
zhangmin <minzhang@vw-mobvoi.com>
2019-05-09 14:30:32 +0800

    commit 1

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.pojo.Person;
 import com.example.demo.repository.Personrepository;
+import com.mobvoi.vw.common.integration.URLCommonService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +18,23 @@ public class TestController {
   @Autowired
   private Personrepository personrepository;
 
+  @Autowired
+  private URLCommonService urlCommonService;
+
   @RequestMapping("/test")
   public Person test(Person person){
+    try {
+      Thread.sleep(10000L);
+    } catch (InterruptedException e) {
+
+    }
     personrepository.save(person);
     return person;
   }
 
   @RequestMapping("/queryByName")
   public List<Person> queryByName(String name){
+    urlCommonService.getForObject("http://127.0.0.2:8085/test",String.class);
     return personrepository.findByName(name);
   }
 }
